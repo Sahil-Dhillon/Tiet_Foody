898f38cdb0722f6c77408bfb70b39bf447d56954
Sahil-Dhillon <sd7082599@gmail.com>
2022-06-23 22:34:59 +0530

    intial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import AppStack from './AppStack';
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <AppStack />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
